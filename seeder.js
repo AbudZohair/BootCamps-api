bd5b2247ca43ee25f9cf1cb0dd8f70423fea50ff
Abdullah Zouhair <abud.zohair@gmail.com>
2021-08-20 22:17:49 +0200

    Add Data Seeder

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+const dotenv = require('dotenv');
+const fs = require("fs");
+const colors = require('colors')
+// Load env vars
+dotenv.config({path: './config/config.env'})
+
+const Bootcamp = require("./models/Bootcamp");
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+
+// Read JSON FILES
+const Bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`)
+);
+
+
+// Import Data to DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(Bootcamps);
+
+    console.log("Data Imported...".green.inverse);
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete Data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log("Data Deleted...".red.inverse);
+    process.exit()
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+if (process.argv[2] === "-i") {
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
