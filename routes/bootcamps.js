b16048e12807682dd8731abe2814d1ddb648a91d
Abdullah Zouhair <abud.zohair@gmail.com>
2021-08-21 19:48:04 +0200

    add route to get bootcamps in readius

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router()
 
-const {createBootcamp,deleteBootcamp,getBootcamp,updateBootcamp, getBootcamps, } = require('../controllers/bootcamps')
+const {createBootcamp,deleteBootcamp,getBootcamp,updateBootcamp, getBootcamps, getBootcampsInRadius} = require('../controllers/bootcamps')
 
 
 
@@ -10,4 +10,5 @@ router.route('/').get(getBootcamps).post(createBootcamp)
 
 router.route('/:id').get(getBootcamp).delete(deleteBootcamp).put(updateBootcamp)
 
+router.get('/radius/:distance/:zipcode', getBootcampsInRadius)
 module.exports = router
