e2b02d993049950fcba403010cbd7fab6a8c3e01
Abdullah Zouhair <abud.zohair@gmail.com>
2021-08-21 21:02:40 +0200

    prettier

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -7,38 +7,69 @@ const geocoder = require("../utils/geocoder");
 // @route     GET /api/v1/bootcamps
 // @access    Public
 exports.getBootcamps = asyncHandler(async (req, res, next) => {
-  let query; 
-  // Copy request Query 
-  const reqQuery = {...req.query}
-  
-  // Fields to Execlude 
-  const removeFields = ['select', 'sort']
+  let query;
+  // Copy request Query
+  const reqQuery = { ...req.query };
+
+  // Fields to Execlude
+  const removeFields = ["select", "sort", "page", "limit"];
 
   // Remove Fields from the reqQuery
-  removeFields.forEach(field => delete reqQuery[field])
+  removeFields.forEach((field) => delete reqQuery[field]);
 
   // Add $ before gt gte lt lte in
-  const  queryStr = JSON.stringify(reqQuery).replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`)
-  query = Bootcamp.find(JSON.parse(queryStr))
+  const queryStr = JSON.stringify(reqQuery).replace(
+    /\b(gt|gte|lt|lte|in)\b/g,
+    (match) => `$${match}`
+  );
+  query = Bootcamp.find(JSON.parse(queryStr));
 
   // Add Select Fields to the Query
-  if(req.query.select){ 
-    const fields = req.query.select.split(',').join(' ')
-    query = query.select(fields)
+  if (req.query.select) {
+    const fields = req.query.select.split(",").join(" ");
+    query = query.select(fields);
   }
 
   // Sort
-  if(req.query.sort){ 
-    const sortBy = req.query.sort.split(',').join(' ');
-    query = query.sort(sortBy)
-  }else{ 
-    query = query.sort('-createdAt')
+  if (req.query.sort) {
+    const sortBy = req.query.sort.split(",").join(" ");
+    query = query.sort(sortBy);
+  } else {
+    query = query.sort("-createdAt");
   }
-  
-  const bootcamps = await query
+
+  // Pagination
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 2;
+  const startIndex = (page - 1) * limit;
+  const endIndex = page * limit;
+  // Find Total Records of the Query
+  const total = await Bootcamp.countDocuments(JSON.parse(queryStr))
+
+  // Add pagination to the Query
+  query = query.skip(startIndex).limit(limit);
+
+  const bootcamps = await query;
+  const pagination = {};
+
+  if (startIndex > 0) {
+    pagination.prev = {
+      page: page - 1,
+      limit,
+    };
+  }
+
+  if (endIndex < total) {
+    pagination.next = {
+      page: page + 1,
+      limit,
+    };
+  }
+
   res.json({
     success: true,
     count: bootcamps.length,
+    pagination,
     data: bootcamps,
   });
 });
@@ -110,31 +141,30 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
   });
 });
 
-
 // @desc      Get Bootcamps within radius
 // @route     GET /api/vi/bootcaps/radius/:distance/:zipcode
 // @access    Public
 
-exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => { 
-  const {zipcode, distance} = req.params
+exports.getBootcampsInRadius = asyncHandler(async (req, res, next) => {
+  const { zipcode, distance } = req.params;
 
-  // get lng, lat from geocoder 
+  // get lng, lat from geocoder
   const [loc] = await geocoder.geocode(zipcode);
-  const {latitude : lat, longitude: lng} = loc
+  const { latitude: lat, longitude: lng } = loc;
   // Calc radius using radians
-  // by dividing the distance by radius of Earth 
-  // Earth Radius  = 3963 miles OR 6378 km 
+  // by dividing the distance by radius of Earth
+  // Earth Radius  = 3963 miles OR 6378 km
   // We will use miles
 
-  const radius  = distance / 3963 
+  const radius = distance / 3963;
 
   const bootcamps = await Bootcamp.find({
-    location : {$geoWithin: {$centerSphere: [[lng, lat], radius]}}
-  })
+    location: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
 
   res.status(200).json({
-    success: true, 
-    count: bootcamps.length, 
-    data: bootcamps
-  })
-})
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps,
+  });
+});
