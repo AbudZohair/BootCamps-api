ab725afb4241675e52452e125bf2c64e0920e98b
Abud Zohair <abud.zohair@gmail.com>
2021-10-21 16:29:03 +0200

    protect bootcamp routes

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const {createBootcamp,deleteBootcamp,getBootcamp,updateBootcamp, getBootcamps, getBootcampsInRadius} = require('../controllers/bootcamps');
-const coursesRouter = require('./courses');
-
+const {
+  createBootcamp,
+  deleteBootcamp,
+  getBootcamp,
+  updateBootcamp,
+  getBootcamps,
+  getBootcampsInRadius,
+} = require("../controllers/bootcamps");
+const { protect } = require("../middleware/auth");
+const coursesRouter = require("./courses");
 
 // Nested Routes (Re-Route)
-router.use('/:bootcampId/courses', coursesRouter)
+router.use("/:bootcampId/courses", coursesRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp)
+router.route("/").get(getBootcamps).post(protect, createBootcamp);
 
-router.route('/:id').get(getBootcamp).delete(deleteBootcamp).put(updateBootcamp)
+router
+  .route("/:id")
+  .get(getBootcamp)
+  .delete(protect, deleteBootcamp)
+  .put(protect, updateBootcamp);
 
-router.get('/radius/:distance/:zipcode', getBootcampsInRadius)
-module.exports = router
+router.get("/radius/:distance/:zipcode", getBootcampsInRadius);
+module.exports = router;
